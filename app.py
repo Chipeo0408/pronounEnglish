28962594a7b8324fa4129553cec96b99d1a5f7f5
Chipeo0408 <chipeo0408@gmail.com>
2025-09-10 11:57:18 +0700

    Mô tả thay đổi

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ with tab_single:
     sourece =  st.text_area("Nguồn:", height=210, placeholder="Overthinking can hurt you.")
     lstData = []
     if sourece:
-        lstData = sourece.split(".") if sourece.find(".") == -1 else [sourece]
-        lstData = [x.strip() for x in lstData if x.strip() if x != ""]
+        lstData = sourece.strip(".").split(".") if sourece.strip(".").find(".") == -1 else [sourece]
+        lstData = [x.strip() for x in lstData if x.strip()]
         
         cola,colb = st.columns(2)
         with cola:
